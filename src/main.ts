e3c2b35b96fe84257ac50d354d088945a68d823f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-03-21 11:26:58 +0100

    Initial

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import {PlaygroundIde} from '../node_modules/playground-elements/playground-ide';
+
+async function main() {
+  const ide = document.querySelector('playground-ide') as PlaygroundIde;
+  const assetUrls = {
+    INDEX_HTML: new URL('./static-defaults/index.html.txt', import.meta.url),
+    MAIN_TS: new URL('./static-defaults/main.ts.txt', import.meta.url),
+  };
+
+  ide.config = {
+    importMap: {
+      imports: {
+        // To speed up RxJS and fix the cross-file extension-less imports which break in ESM.
+        'rxjs/operators': 'https://unpkg.com/@esm-bundle/rxjs/esm/es2015/rxjs-operators.min.js',
+        rxjs: 'https://unpkg.com/@esm-bundle/rxjs/esm/es2015/rxjs.min.js',
+      },
+    },
+    files: {
+      'main.ts': {
+        content: await (await fetch(assetUrls.MAIN_TS.toString())).text(),
+      },
+      'index.html': {
+        content: await (await fetch(assetUrls.INDEX_HTML.toString())).text(),
+      },
+    },
+  };
+
+  ide.resizable = true;
+  ide.lineNumbers = true;
+  ide.editableFileSystem = true;
+}
+
+main().catch(e => console.error(e));
